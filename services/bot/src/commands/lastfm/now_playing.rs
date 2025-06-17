c46862254e0488729d2edc38f0d93bb1b7a88718
chewawi <antoteje2004@gmail.com>
2025-06-16 22:22:10 -0300

    refactor: remove now_playing card

--- services/bot/src/commands/lastfm/now_playing.rs
+++ services/bot/src/commands/lastfm/now_playing.rs
@@ -1,13 +1,10 @@
+use std::vec;
 use crate::core::structs::{Context, Error};
-use crate::images::lastfm::now_playing_card::now_playing_card;
 use common::utils::truncate_text;
 use database::model::{colors::Colors, lastfm::Lastfm};
 use lumi::serenity_prelude as serenity;
 use lumi::CreateReply;
-use ::serenity::builder::{
-    CreateAttachment, CreateComponent, CreateContainer, CreateMediaGallery, CreateMediaGalleryItem,
-    CreateSeparator, CreateTextDisplay, CreateUnfurledMediaItem, Spacing,
-};
+use sqlx::types::chrono::Utc;
 
 #[lumi::command(
     slash_command,
@@ -16,90 +13,133 @@ use ::serenity::builder::{
     description_localized("en-US", "Display your current song from Last.fm")
 )]
 pub async fn now_playing(ctx: Context<'_>) -> Result<(), Error> {
+    let _ = ctx.defer_or_broadcast().await;
     let data = ctx.data();
     let user_id = ctx.author().id.get();
 
-    let session = Lastfm::get(&data.db, user_id)
-        .await?
-        .ok_or("Link your account with /login")?;
+    let session_result = Lastfm::get(&data.db, user_id).await;
 
-    let (track_opt, user) = tokio::try_join!(
-        data.lastfm.get_current_track(session),
-        data.lastfm.get_user_info(user_id)
-    )?;
+    let session = match session_result {
+        Ok(Some(session)) => session,
+        Ok(None) => return Err("Link your account with /login".into()),
+        Err(e) => return Err(Error::from(e)),
+    };
+
+    // Get the user to check when they were last updated
+    let user = match data.db.get_user(user_id as i64).await {
+        Ok(user) => user,
+        Err(e) => return Err(Error::from(e)),
+    };
+
+    // Check if the user needs a quick update (more than 3 minutes since last update)
+    let needs_update = match user.last_updated {
+        Some(last_updated) => {
+            let now = Utc::now();
+            let minutes_since_update = (now - last_updated).num_minutes();
+            minutes_since_update >= 3
+        },
+        None => true, // No last_updated timestamp means we definitely need an update
+    };
+
+    // If the user needs an update, trigger a quick sync
+    if needs_update {
+        // Use force=false to let the sync_scrobbles method determine the update type
+        if let Err(e) = data.lastfm.sync_scrobbles(user_id, false).await {
+            // Log the error but continue with potentially stale data
+            println!("Warning: Failed to sync scrobbles: {}", e);
+        }
+    }
+
+    let track_future = data.lastfm.get_current_track(session.clone());
+    let user_future = data.lastfm.get_user_info(user_id);
+
+    let (track_opt, user) = tokio::try_join!(track_future, user_future)?;
 
     let track = track_opt.ok_or("No music currently playing")?;
-    let (small_url, _, extra_url) = data.lastfm.get_image_urls(&track.image)?;
 
-    let track_info = data
-        .lastfm
-        .get_track_info(user_id, &track.artist.text, &track.name)
-        .await?;
+    // Handle the case where no images are found
+    let default_image_url = "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png";
+    let (small_url, medium_url) = match data.lastfm.get_image_urls(&track.image) {
+        Ok((small, medium, _)) => (small, medium),
+        Err(err) => {
+            // Log the error but continue with default values
+            println!("Warning: Could not get image URLs: {}", err);
+            (default_image_url, default_image_url)
+        }
+    };
 
-    let card_bytes = now_playing_card(
-        &track.name,
-        &track.artist.text,
-        track
-            .album
-            .as_ref()
-            .map(|a| a.text.as_str())
-            .unwrap_or("Unknown Album"),
-        extra_url,
-    )
-    .await?;
+    let track_info_future = data.lastfm.get_track_info(
+        user_id, 
+        &track.artist.text, 
+        &track.name
+    );
 
-    let container = create_container(ctx, &track, &track_info, &user, small_url).await?;
+    let color_future = Colors::get(
+        &data.db.cache,
+        data.http_client.clone(),
+        small_url
+    );
+
+    // Handle potential errors from the futures
+    let (track_info, color_result) = match tokio::try_join!(track_info_future, color_future) {
+        Ok(result) => result,
+        Err(err) => {
+            println!("Warning: Error getting track info or color: {}", err);
+            // Return default values
+            (
+                lastfm::TrackInfo { 
+                    playcount: "0".to_string(), 
+                    userplaycount: "0".to_string() 
+                },
+                None
+            )
+        }
+    };
+
+    let container_future = async {
+        let color = color_result
+            .map(|c| serenity::Colour::from_rgb(c[0], c[1], c[2]))
+            .unwrap_or(serenity::Colour::DARK_GREY);
+
+        let text_display_content = format!(
+            "**[{}]({})**\n-# {} - {}",
+            truncate_text(&track.name, 40),
+            track.url,
+            truncate_text(&track.artist.text, 30),
+            track
+                .album
+                .as_ref()
+                .map(|a| truncate_text(&a.text, 50))
+                .unwrap_or_else(|| "Unknown Album".to_string()),
+        );
+
+        serenity::CreateContainer::new(vec![
+            serenity::CreateComponent::Section(
+                serenity::CreateSection::new(
+                    vec![serenity::CreateSectionComponent::TextDisplay(serenity::CreateTextDisplay::new(text_display_content))],
+                    serenity::CreateSectionAccessory::Thumbnail(
+                        serenity::CreateThumbnail::new(serenity::CreateUnfurledMediaItem::new(medium_url))
+                    ),
+                )
+            ),
+            serenity::CreateComponent::Separator(serenity::CreateSeparator::new(true).spacing(serenity::Spacing::Small)),
+            serenity::CreateComponent::TextDisplay(serenity::CreateTextDisplay::new(format!(
+                "-# plays: `{}` | scrobbles: `{}`",
+                track_info.userplaycount, user.playcount
+            ))),
+        ])
+        .accent_color(color.0)
+    };
+
+    let container = container_future.await;
 
     ctx.send(
         CreateReply::default()
             .flags(serenity::MessageFlags::IS_COMPONENTS_V2)
-            .components(&[CreateComponent::Container(container)])
-            .attachment(CreateAttachment::bytes(card_bytes, "now_playing.png"))
+            .components(&[serenity::CreateComponent::Container(container)])
             .reply(true),
     )
     .await?;
 
     Ok(())
-}
-
-async fn create_container<'a>(
-    ctx: Context<'_>,
-    track: &'a lastfm::Track,
-    track_info: &'a lastfm::TrackInfo,
-    user: &'a lastfm::UserInfo,
-    image_url: &str,
-) -> Result<CreateContainer<'a>, Error> {
-    let color = Colors::get(
-        &ctx.data().db.cache,
-        ctx.data().http_client.clone(),
-        image_url,
-    )
-    .await?
-    .map(|c| serenity::Colour::from_rgb(c[0], c[1], c[2]))
-    .unwrap_or(serenity::Colour::DARK_GREY);
-
-    let text_display_content = format!(
-        "**[{}]({})**\n-# {} - {}",
-        truncate_text(&track.name, 40),
-        track.url,
-        truncate_text(&track.artist.text, 30),
-        track
-            .album
-            .as_ref()
-            .map(|a| truncate_text(&a.text, 50))
-            .unwrap_or_else(|| "Unknown Album".to_string()),
-    );
-
-    Ok(CreateContainer::new(vec![
-        CreateComponent::TextDisplay(CreateTextDisplay::new(text_display_content)),
-        CreateComponent::MediaGallery(CreateMediaGallery::new(vec![CreateMediaGalleryItem::new(
-            CreateUnfurledMediaItem::new("attachment://now_playing.png"),
-        )])),
-        CreateComponent::Separator(CreateSeparator::new(true).spacing(Spacing::Small)),
-        CreateComponent::TextDisplay(CreateTextDisplay::new(format!(
-            "-# plays: `{}` | scrobbles: `{}`",
-            track_info.userplaycount, user.playcount
-        ))),
-    ])
-    .accent_color(color.0))
 }
